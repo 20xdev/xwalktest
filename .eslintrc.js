98f24ef9fa7f13f29adb4453cfc40fefc1910731
bhupesh-99 <pipley@adobe.com>
2024-07-25 13:06:08 +0530

    adding image

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,20 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'xwalk/max-cells': ['error', {
+      '*': 4,
+      // 'a-specific-model': 4,
+      // 'another-specific-model': 2,
+    }],
   },
 };
+
+// {
+//   rules: {
+//     'xwalk/max-cells': ['error', {
+//       '*': 3,
+//       'a-specific-model': 4,
+//       'another-specific-model': 2,
+//     }]
+//   }
+// }
